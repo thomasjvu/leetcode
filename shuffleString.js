39ad757f006b6518bb536b796aac83e316925ff8
Thomas Vu <thomasjvu@gmail.com>
2023-03-19 17:58:10 -0400

    Add Leetcode solution

---
+++ shuffleString.js
@@ -0,0 +1,48 @@
+// LeetCode # 1528. Shuffle String
+/* Instructions
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+ 
+
+Example 1:
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+ 
+
+Constraints:
+
+    s.length == indices.length == n
+    1 <= n <= 100
+    s consists of only lowercase English letters.
+    0 <= indices[i] < n
+    All values of indices are unique.
+
+*/
+
+// Solution
+const restoreString = (s, indices) => {
+
+    // create an empty array
+    const result = []
+
+    // loop through each element in the string
+    for (let i = 0; i < s.length; i++) {
+        // insert the character at current index of the normal string at specified index of the result array using the indicies array
+        result[indices[i]] = s[i]
+    }
+
+    // return the result array joined as a string
+    return result.join('')
+
+}
