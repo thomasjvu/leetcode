3a9ff840f1ea194bb145a54e4a8ad8fd927834dd
Thomas Vu <thomasjvu@gmail.com>
2023-03-22 18:53:40 -0400

    add leetcode solution

---
+++ differenceBetweenElementSumAndDigitSumOfAnArray.js
@@ -0,0 +1,59 @@
+// LeetCode #2535. Difference Between Element Sum and Digit Sum Of An Array
+/* Instructions
+You are given a positive integer array nums.
+
+    The element sum is the sum of all the elements in nums.
+    The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+ 
+
+Example 1:
+
+Input: nums = [1,15,6,3]
+Output: 9
+Explanation: 
+The element sum of nums is 1 + 15 + 6 + 3 = 25.
+The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 0
+Explanation:
+The element sum of nums is 1 + 2 + 3 + 4 = 10.
+The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 2000
+    1 <= nums[i] <= 2000
+
+*/
+
+// Solution
+const differenceOfSum = (nums) => {
+    // use .reduce() method to sum all numbers in array
+    const elementSum = nums.reduce((acc, curr) => acc + curr, 0)
+
+    // get digit sum by concatenating each array element into a single string
+    // then loop through each character in string and add numeric value to digitSum
+    let digitSum = 0
+    let digits = ''
+
+    for (let i = 0; i < nums.length; i++) {
+        digits += nums[i].toString()
+    }
+    for (let i = 0; i < digits.length; i++) {
+        digitSum += Number(digits[i])
+    }
+
+    return Math.abs(digitSum - elementSum)
+}
