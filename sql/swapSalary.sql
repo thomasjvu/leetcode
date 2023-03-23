f63ee027af389a6855be83cb1b77ebea756a3fd8
Thomas Vu <thomasjvu@gmail.com>
2023-03-23 13:33:19 -0400

    Add LeetCode solution

---
+++ sql/swapSalary.sql
@@ -0,0 +1,71 @@
+-- LeetCode #627. Swap Salary
+/* Instructions
+Table: Salary
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+
+ 
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+Output: 
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+Explanation: 
+(1, A) and (3, C) were changed from 'm' to 'f'.
+(2, B) and (4, D) were changed from 'f' to 'm'.
+
+*/
+
+-- Solution 1
+UPDATE salary
+SET 
+    sex = CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END
+
+-- Solution 2
+UPDATE salary 
+SET sex = IF (sex = "m", "f", "m")
+
+-- Explanation
+-- Use the UPDATE keyword to dynamically set a value to the chosen table
+-- Use the SET keyword to choose the column to update
+-- Use a conditional statement: either a case-statement (CASE... WHEN condition1 ... THEN result 1 ... ELSE result ... END) 
+-- Or an if-statement (condition, value_if_true, value_if_false)
+-- To update the column's values based on a certain condition
