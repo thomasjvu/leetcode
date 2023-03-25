e8d05b00cde682a69d32259f2bd8fc44dcbaadb3
Thomas Vu <thomasjvu@gmail.com>
2023-03-25 15:21:31 -0400

    Add LeetCode solution

---
+++ checkIfStringIsAPrefixOfArray.js
@@ -0,0 +1,53 @@
+// LeetCode #1961. Check If String Is a Prefix of Array
+/* Instructions
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+Return true if s is a prefix string of words, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+Output: true
+Explanation:
+s can be made by concatenating "i", "love", and "leetcode" together.
+
+Example 2:
+
+Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
+Output: false
+Explanation:
+It is impossible to make s using a prefix of arr.
+
+ 
+
+Constraints:
+
+    1 <= words.length <= 100
+    1 <= words[i].length <= 20
+    1 <= s.length <= 1000
+    words[i] and s consist of only lowercase English letters.
+
+*/
+
+// Solution
+const isPrefixString = (s, words) => {
+
+    // set k equal to the length of the array
+    let k = words.length
+    // set an empty string to hold prefix concatenation
+    let word = ''
+
+    // loop through each element in array
+    for (let i = 0; i < k; i++) {
+        // concatenate each word onto string, and if at any point the string is equal to s, s is a prefix
+        // therefore, we would return true
+        words += words[i]
+        if (word === s) return true
+    }
+    // otherwise, return false
+    return false
+}
