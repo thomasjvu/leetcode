ba6e2a99c40a0ce8df0352086e7369b055fa017a
Thomas Vu <thomasjvu@gmail.com>
2023-04-27 00:15:04 -0400

    Add LeetCode solution

---
+++ subtractTheProductAndSumOfDigitsOfAnInteger.js
@@ -0,0 +1,49 @@
+// LeetCode #1281. Subtract the Product and Sum of Digits of an Integer
+/* Instructions
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+
+ 
+
+Constraints:
+
+    1 <= n <= 10^5
+*/
+
+// Solution (Brute Force)
+const subtractProductAndSum = (n) => {
+
+    // initialize sum and product values
+    let product = 1
+    let sum = 0
+    // get string value of n
+    let strN = String(n)
+
+    // loop through length of string, and for each character, add or multiply numeric value to initialized sum or product
+    for (let i = 0; i < strN.length; i++) {
+        sum += (+strN[i])
+        product *= (+strN[i])
+    }
+
+    // return difference between product and sum
+    return product - sum
+}
