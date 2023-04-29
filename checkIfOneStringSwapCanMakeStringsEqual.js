be7a2475096e7cfab6ccb82714b93df5a0eec340
Thomas Vu <thomasjvu@gmail.com>
2023-04-29 13:33:08 -0400

    Add LeetCode solution

---
+++ checkIfOneStringSwapCanMakeStringsEqual.js
@@ -0,0 +1,60 @@
+// LeetCode #1790. Check if One String Swap Can Make Strings Equal
+/* Instructions
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+
+ 
+
+Constraints:
+
+    1 <= s1.length, s2.length <= 100
+    s1.length == s2.length
+    s1 and s2 consist of only lowercase English letters.
+
+*/
+
+// Solution
+function areAlmostEqual (s1, s2) {
+    
+    // if strings are the same then return true automatically
+    if (s1 === s2) return true 
+
+    // create array of mismatched indexes
+    let mismatches = []
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            mismatches.push(i)
+        }
+    }
+
+    // swap only works if there are 2 mismatches (1 swap)
+    if (mismatches.length !== 2) {
+        return false
+    }
+
+    // swap only works if the mismatches are of the same values
+    return (s1[mismatches[0]] === s2[mismatches[1]] && s1[mismatches[1]] === s2[mismatches[0]])
+    
+};
