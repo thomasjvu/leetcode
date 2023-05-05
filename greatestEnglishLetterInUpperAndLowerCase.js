2a584dea0f66572c2a719d4a6645a01feb00e18e
Thomas Vu <thomasjvu@gmail.com>
2023-05-05 16:34:12 -0400

    Add LeetCodes solution

---
+++ greatestEnglishLetterInUpperAndLowerCase.js
@@ -0,0 +1,59 @@
+// LeetCode #2309. Greatest English Letter in Upper and Lower Case
+/* Instructions
+Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. The returned letter should be in uppercase. If no such letter exists, return an empty string.
+
+An English letter b is greater than another letter a if b appears after a in the English alphabet.
+
+ 
+
+Example 1:
+
+Input: s = "lEeTcOdE"
+Output: "E"
+Explanation:
+The letter 'E' is the only letter to appear in both lower and upper case.
+
+Example 2:
+
+Input: s = "arRAzFif"
+Output: "R"
+Explanation:
+The letter 'R' is the greatest letter to appear in both lower and upper case.
+Note that 'A' and 'F' also appear in both lower and upper case, but 'R' is greater than 'F' or 'A'.
+
+Example 3:
+
+Input: s = "AbCdEfGhIjK"
+Output: ""
+Explanation:
+There is no letter that appears in both lower and upper case.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s consists of lowercase and uppercase English letters.
+
+*/
+
+// Solution
+function greatestLetter (s) {
+
+    let greatestValue = 0
+    let greatestUpper = ''
+
+    for (let i = 0; i < s.length; i++) {
+        // check if string has character in both uppercase and lowercase, and check if value is greater than current value
+        if (s.includes(s[i].toLowerCase()) && s.includes(s[i].toUpperCase()) && s.toUpperCase().charCodeAt(i) >= greatestValue) {
+            greatestUpper = s[i].toUpperCase()
+            greatestValue = greatestUpper.charCodeAt(0)
+        }
+    }
+
+    // return greatest uppercase letter
+    return greatestUpper
+    
+};
+
+
