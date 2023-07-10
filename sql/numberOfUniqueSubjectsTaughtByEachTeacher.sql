a63f5aab11f6e92c6168ba58bebdb93f87831dec
Thomas Vu <thomasjvu@gmail.com>
2023-07-10 14:49:36 -0400

    Add Leetcode solution

---
+++ sql/numberOfUniqueSubjectsTaughtByEachTeacher.sql
@@ -0,0 +1,67 @@
+-- LeetCode #2356. Number of Unique Subjects Taught by Each Teacher
+/* Instructions
+Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key for this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+
+ 
+
+Calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+The result format is shown in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+Explanation: 
+Teacher 1:
+  - They teach subject 2 in departments 3 and 4.
+  - They teach subject 3 in department 3.
+Teacher 2:
+  - They teach subject 1 in department 1.
+  - They teach subject 2 in department 1.
+  - They teach subject 3 in department 1.
+  - They teach subject 4 in department 1.
+
+*/
+
+-- Solution
+SELECT teacher_id, COUNT(DISTINCT(subject_id)) as cnt
+FROM Teacher
+GROUP BY teacher_id
+
+-- Explanation
+-- Use the COUNT(DISTINCT) query to get all of the unique subjects taught
+-- Use the GROUP BY query to group all of the unique subjects for every teacher
+-- We are SELECTING the teacher_id and unique subject_id FROM the Teacher table
